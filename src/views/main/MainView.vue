d7bf7e4ced23bac4df36bf9bc43250ec748916f7
yss9358 <93y2s@naver.com>
2024-03-25 18:10:11 +0900

    수정

--- src/views/main/MainView.vue
+++ src/views/main/MainView.vue
@@ -11,7 +11,7 @@
                 <li><a href="" class="btn_s">회원정보수정</a></li>
             </ul>
             <ul v-if="this.$store.state.authUser == null">
-                <li><a href="" class="btn_s">로그인</a></li>
+                <li><router-link to="/user/loginform" class="btn_s">로그인</router-link></li>
                 <li><a href="" class="btn_s">회원가입</a></li>
             </ul>
         </div>
@@ -40,13 +40,15 @@
                     <div id="greetings">
                         <p class="text-xlarge">
                             <span class="bold">안녕하세요!!<br>
-                            황일영의 MySite에 오신 것을 환영합니다.<br>
+                            <span v-if="this.$store.state.authUser != null">{{this.$store.state.authUser.name}}의 MySite에 오신 것을 환영합니다.</span>
+                            <span v-if="this.$store.state.authUser == null">MySite에 오신 것을 환영합니다.</span>
+                            <br>
                             <br>
                             이 사이트는 웹 프로그램밍 실습과제 예제 사이트입니다.<br>
                             </span>
                             <br>
                             사이트 소개, 회원가입, 방명록, 게시판으로 구성되어 있으며<br>
-                            jsp&serlvet(모델2) 방식으로 제작되었습니다.<br>
+                            SpringBoot & Vue-js 방식으로 제작되었습니다.<br>
                             <br>
                             자바 수업 + 데이터베이스 수업 + 웹프로그래밍 수업<br>
                             배운 거 있는거 없는 거 다 합쳐서 만들어 놓은 사이트 입니다.<br>
