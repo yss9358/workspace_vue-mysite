30b1d6606c6433e020d39dacd01150d84e291bec
yss9358 <93y2s@naver.com>
2024-04-08 16:39:31 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir : 'C:/javaStudy/workspace_sb/api-mysite/src/main/resources/static'
 })
